c2d12177d25f666ba8382094c0567283a0befa68
priyam1103 <priyampoddar89@gmail.com>
2020-11-10 23:25:50 +0530

    first commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const User = require("./model/user");
 
 
 const { user } = require("./utils/Mailer");
-const PORT = 3006;
+const PORT = process.env.PORT || 3006;
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
