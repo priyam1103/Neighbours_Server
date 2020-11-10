642c1db65fdc8738a8ddfecd334db0c427b36086
priyam1103 <priyampoddar89@gmail.com>
2020-11-10 23:10:10 +0530

    first commit

--- service/db.js
+++ service/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("./config");
 function connectDb() {
-  return mongoose.connect(config.MONGODB_URI, {
+  return mongoose.connect("mongodb+srv://priyam1103:priyam7035@cluster0.cis4d.mongodb.net/Neighbours?retryWrites=true&w=majority", {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
